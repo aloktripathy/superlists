e85964d303b220039a2e42ed3bd73a7f8a907648
aloktripathy <alokrant@gmail.com>
2015-05-21 09:35:36 +0530

    Testing browser

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,24 @@
 __author__ = 'AL299758'
 
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8080')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        print('setting up------------------------')
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(time_to_wait=3)
+
+    def tearDown(self):
+        print('tearing down---------------------')
+        self.browser.quit()
+
+    def test_title(self):
+        print('now testing----------------------')
+        self.browser.get('http://localhost:8080')
+        self.assertIn('To-Do', self.browser.title)
+        # self.fail('Finish the test')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
